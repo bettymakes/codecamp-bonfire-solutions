369342b17cf109d80c31b42385904e5910222122
betty <makewithbetty@gmail.com>
2015-04-29 17:39:18 -0400

    Included tests passed for documentation & fixed formatting

--- 01-meet-bonfire.js
+++ 01-meet-bonfire.js
@@ -2,14 +2,17 @@
 // [01] Meet Bonfire
 
 /*
+  Instructions:
   Your goal is to fix the failing test.
-
   First, run all the tests by clicking "Run code" or by pressing Control + Enter
-
   The failing test is in red. Fix the code so that all tests pass. Then you 
   can move on to the next Bonfire.
 
   Make this function return true no matter what.
+
+  Tests:
+  expect(meetBonfire()).to.be.true;
+  expect(meetBonfire()).to.be.a("boolean");
 */
 
 
